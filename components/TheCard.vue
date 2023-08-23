cfb856b4d2e1fdc125c67d2b7dab50237f697083
kim <kimberlypangilinan2001@gmail.com>
2023-08-23 11:23:17 +0800

    home page modified

--- components/TheCard.vue
+++ components/TheCard.vue
@@ -4,10 +4,9 @@ defineProps(['title','pic', 'desc'])
 
 
 <template>
-    <div>
-        <h4>{{ title }}</h4>
-        <img :src="pic" alt=""/>
-        <p>{{ desc  }}</p>
+    <div class="grid place-items-center gap-4">
+        <img class="w-[110px]" :src="pic" alt=""/>
+        <h4 class="text-lg">{{ title }}</h4>
+        <p class="text-base font-medium w-8/12">{{ desc  }}</p>
     </div>
-  
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 
 <template lang="">
-    <div class="w-100 h-screen grid place-items-center ">
+    <div class="w-100 h-[80vh] grid place-items-center ">
        <h1 class="font-sans text-lg font-extrabold">Need something done?</h1>
-       <TheCard title="Post a Job" pic="https://static.vecteezy.com/system/resources/previews/013/132/405/original/bag-find-job-job-website-online-portfolio-flat-color-icon-icon-banner-template-free-vector.jpg" />
+       <div class="flex justify-between gap-[103px] px-[174px]">
+        <TheCard 
+        title="Post a Job" 
+        pic="/img/job.png" 
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <TheCard 
+        title="Pay safely" 
+        pic="/img/pay.png" 
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <TheCard 
+        title="Post a Job" 
+        pic="/img/help.png" 
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+       </div>
+       
 
     </div>
 </template>
