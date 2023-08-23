0018c7f524bfba4669deda07611a6b8ca717bb66
kim <kimberlypangilinan2001@gmail.com>
2023-08-23 09:49:09 +0800

    initializaation and adding of components/pages/ routing

---
+++ components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="/logo.png" alt="logo">
+        <nav>
+        <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/accounts">Accounts</NuxtLink></li>
+            <li><NuxtLink to="/"> Sales</NuxtLink></li>
+        </ul>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-active{
+        border-bottom: 2px solid red;
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary-red': '#E42323'
+      }
+  },
+  plugins: [],
+}
+}
+
