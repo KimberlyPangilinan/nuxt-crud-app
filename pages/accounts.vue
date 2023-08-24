ac1b3746498f3a0469a02cff95598471f13fb573
kim <kimberlypangilinan2001@gmail.com>
2023-08-24 16:54:39 +0800

    fixed some design

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -2,7 +2,7 @@
 
 <template lang="">
     <main class="mt-[12em] w-100 min-h-screen flex flex-col px-16 gap-4">
-        <!-- search filters and buttons -->
+<!-- search filters and buttons -->
         <div class="flex justify-between fixed top-20 left-0 px-16 w-screen bg-white">
             <div class="flex gap-4 py-8  ">
                 <div class="border rounded-sm max-w-fit py-2 px-4 flex justify-center ">
@@ -16,7 +16,7 @@
                         />
                     <button><Icon class="text-gray-400" icon="carbon:search" /></button>
                 </div>
-                <div class="border rounded-sm max-w-fit flex justify-center flex-row-reverse ">
+                <div class="border rounded-md max-w-fit flex justify-center flex-row-reverse ">
                     <input
                         class="py-2 px-4 placeholder:text-xs placeholder:text-slate-400 outline-none "
                         type="text"
@@ -25,7 +25,7 @@
                         placeholder="department"
                         v-model="filterDept"
                         />
-                    <button class="bg-primary-red px-4"><Icon class="text-white" icon="iconoir:filter" /></button>
+                    <button class="bg-primary-red px-4 rounded-sm"><Icon class="text-white" icon="iconoir:filter" /></button>
                 </div>
             </div>
             <div class="py-8">    
@@ -33,7 +33,7 @@
                     class="bg-primary-red text-white py-3 px-6 flex items-center gap-2 text-sm rounded"
                     @click="isOpen = !isOpen"
                     id="new"
-                 ><Icon icon="ic:baseline-plus" /> New</button>
+                 ><Icon icon="ic:baseline-plus"/>New</button>
             </div>
         </div>
 <!-- this is the new employee form-->
@@ -60,83 +60,110 @@
                     </div>
                 </div>
                 <div class="flex flex-col justify-center gap-2 w-[20%] px-12">
-                    <VButton color="green" id="Add" @click="addNewItem"/>
+                    <VButton color="green" id="Add" @click="handleAddItem"/>
                     <VButton color="#D9D9D9" id="Cancel"/>
                 </div>
             </form>
         </div>
 <!-- table -->
-        
-        <div class=" flex flex-col gap-6">
-            <h2 class="font-semibold flex w-[100%] items-center gap-10">Employees Account <hr class="grow bg-red-100 h-[0.3rem]"></h2>
-            <div class="flex gap-4 items-start">
-                <table class="table-auto grow">
-                    <thead>
-                        <tr class="text-left text-gray-500 font-medium text-xs h-16">
-                        <th class="p-3"><input type="checkbox"/></th>
-                        <th class="p-3">File Name/ID No.</th>
-                        <th class="p-3">Date</th>
-                        <th class="p-3">Email</th>
-                        <th class="p-3">Department</th>
-                        <th class="p-3">Active</th>
-                        <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr @click="() => handleRowClick(item)" v-for="item in filteredItemsByName" class=" odd:bg-gray-100 text-gray-500 hover:bg-red-50">
-                        <td class="p-3 bg-white"><input type="checkbox"/></td>
-                        <td class="flex gap-4 bg-white p-3 min-w-[300px] ">
-                            <img :src="item.image" class="w-[47px]"/> 
-                            <h4 class="flex flex-col text-black">
-                                {{ item.fName }} {{item.mName}} {{ item.lName }} 
-                                <span class="text-sm ">{{item.id}}</span>
-                            </h4>
-                        </td>
-                        <td class="p-3">{{item.date}}</td>
-                        <td class="p-3">{{item.email}}</td>
-                        <td class="p-3">{{ item.department}} </td>
-                        <td class="p-3 font-semibold" :style="{ color: item.isActive ? 'green' : 'red' }">{{ item.isActive? 'Active': 'Deactivated' }}</td>
-                        <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div v-if="clickedRow" class="flex flex-col min-w-[340px] max-w-[340px] bg-[#F8F7F7] p-8">
-                    <div class="flex p-4">
-                        <img :src="clickedRow.image" class="w-[99px] aspect-square object-cover"/>
-                        <div>
-                            <span class="text-[#00A2FD]">Upload image</span>
-                            {{ clickedRow.fName }} {{clickedRow.mName}} {{ clickedRow.lName }} 
-                        </div>
-                    </div>
-                    <div>
-                        <div class="flex flex-col">
-                            <label>First Name</label>
-                            <input type="text" v-model="clickedRow.fName" 
-                            class="px-2 py-1 bg-[#F6F6F6] text-[#766E6E] rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" 
-                            />
-                        </div>
-                        <div class="flex flex-col">
-                            <label>Middle</label>
-                            <input type="text" v-model="clickedRow.mName" 
-                            class="px-2 py-1 bg-[#F6F6F6] text-[#766E6E] rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" 
-                            />
-                        </div>
-                        <div class="flex flex-col">
-                            <label>Last Name</label>
-                            <input type="text" v-model="clickedRow.lName" 
-                            class="px-2 py-1 bg-[#F6F6F6] text-[#766E6E] rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" 
-                            />
-                        </div>
-                        <div class="flex flex-col">
-                            <label>Email</label>
-                            <input type="email" v-model="clickedRow.email" 
-                            class="px-2 py-1 bg-[#F6F6F6] text-[#766E6E] rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" 
-                            />
-                        </div>
-                    </div>
-                </div>
+<div class="flex gap-4">
+  <div class="flex flex-col gap-6 grow">
+    <h2 class="font-semibold flex w-[100%] items-center gap-10">
+      Employees Account
+      <hr class="grow bg-red-100 h-[0.3rem]">
+    </h2>
+    <div class="flex gap-4 items-start ">
+      <table class="table-auto grow text-[#667085]  ">
+        <thead>
+          <tr class="text-left text-gray-500 font-medium text-xs h-16">
+            <th class="p-3"><input type="checkbox"/></th>
+            <th class="p-3">File Name/ID No.</th>
+            <th class="p-3">Date</th>
+            <th class="p-3">Email</th>
+            <th class="p-3">Department</th>
+            <th class="p-3">Active</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            @click="() => handleRowClick(item)"
+            v-for="item in filteredItemsByName"
+            class="odd:bg-gray-100 text-gray-500 hover:bg-red-50"
+          >
+            <td class="p-3 bg-white"><input type="checkbox"/></td>
+            <td class="flex gap-4 bg-white p-3 min-w-[300px]">
+              <img :src="item.image" class="w-[47px]"/> 
+              <h4 class="flex flex-col text-black">
+                {{ item.fName }} {{ item.mName }} {{ item.lName }} 
+                <span class="text-sm text-[#667085]">{{ item.id }}</span>
+              </h4>
+            </td>
+            <td class="p-3">{{ item.date }}</td>
+            <td class="p-3">{{ item.email }}</td>
+            <td class="p-3">{{ item.department }} </td>
+            <td class="p-3 font-semibold" :style="{ color: item.isActive ? 'green' : 'red' }">
+              {{ item.isActive ? 'Active' : 'Deactivated' }}
+            </td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  
+  <div v-if="clickedRow" class="flex flex-col max-w-[380px] bg-[#F8F7F7] p-6 max-h-[70vh]">
+    <div class="flex gap-1 justify-end">
+        <button class="p-2 rounded-md bg-yellow-400" ><Icon class="text-white" icon="grommet-icons:power-reset" /></button>
+        <button class="p-2 rounded-md bg-green-600" @click="isDisabled=!isDisabled"><Icon class="text-white" icon="gg:pen" /></button>
+        <button class="p-2 rounded-md bg-red-600 " @click=" () => handleDelete(item)"><Icon class="text-white" icon="ant-design:delete-outlined" /></button>
+    </div>
+    <div class="flex p-4 gap-4 items-center grow">
+      <img :src="clickedRow.image" class="w-[99px] aspect-square object-cover"/>
+      <div class="text-xs font-medium flex flex-col">
+        <span class="text-[#00A2FD] ">Upload image</span>
+        {{ clickedRow.fName }} {{ clickedRow.mName }} {{ clickedRow.lName }} 
+      </div>
+    </div>
+    <div class="flex justify-between gap-4">
+        <div class="flex flex-col gap-4">
+            <div class="flex flex-col">
+                <label class="text-xs">First Name</label>
+                <input type="text" v-model="clickedRow.fName" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
+            </div>
+            <div class="flex flex-col">
+                <label class="text-xs">Middle</label>
+                <input type="text" v-model="clickedRow.mName" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
+            </div>
+            <div class="flex flex-col">
+                <label class="text-xs">Last Name</label>
+                <input type="text" v-model="clickedRow.lName" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
+            </div>
+            
+        </div>
+        <div class="flex flex-col gap-4 ">
+            <div class="flex flex-col">
+                <label class="text-xs">Email</label>
+                <input type="email" v-model="clickedRow.email" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
+            </div>
+            <div class="flex flex-col">
+                <label class="text-xs">Department</label>
+                <input type="email" v-model="clickedRow.department" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
+            </div>
+            <div class="flex flex-col">
+                <label class="text-xs">ID No.</label>
+                <input type="email" v-model="clickedRow.id" :disabled="isDisabled" class="p-2 bg-[#F6F6F6] disabled:text-[#766E6E] text-xs rounded-md border  border-slate-300 placeholder-slate-400 focus:outline-none placeholder:text-slate-400" />
             </div>
         </div>
+      
+    </div>
+    <div class="pt-4 flex gap-2 justify-end grow align-bottom items-end">
+        <button class="px-4 py-3 w-[80px] h-[46px] rounded-sm text-xs text-white bg-[#D9D9D9]" @click="handleCancel">Cancel</button>
+        <button class="px-4 py-3 w-[80px] h-[46px] rounded-sm text-xs text-white bg-[#308F00]" @click="handleUpdate">Update</button>
+    </div>
+  </div>
+</div>
+
     </main>
 </template>
 
@@ -195,6 +222,7 @@ const searchText = ref("")
 const filterDept = ref("")
 const clickedRow=ref(null)
 
+const isDisabled=ref(true)
 // Search by name
 const filteredItemsByName = computed(() => {
     if (!searchText.value) {
@@ -227,7 +255,7 @@ const addItem = (newItem) => {
     nuxtStorage.localStorage.setData('employeeItems',JSON.stringify(items.value), 1, 'h');
 };
 
-const addNewItem = () => {
+const handleAddItem = () => {
     const newItem = {
         image: 'img',
         fName: fName,
@@ -245,7 +273,17 @@ const addNewItem = () => {
     isOpen.value = !isOpen.value; 
 };
 
+const handleCancel = ()=> {
+    clickedRow.value = null
+}
 
+const handleDelete = (item)=> {
+    items.filter(emp => emp.id != item.id)
+}
+
+const handleUpdate = ()=> {
+
+}
 
 
 </script>
